44f6d828c34d0ac18976b0b3cec78708ca1cf946
Nima Shahbazi <nimashahbazi524@gmail.com>
2025-05-28 12:01:08 +0330

    Fix build error

--- src/faq/faq.module.ts
+++ src/faq/faq.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FaqController } from './faq.controller';
 import { FaqService } from './faq.service';
 import { Faq } from './entities/faq.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Faq])],
+  imports: [
+    TypeOrmModule.forFeature([Faq]),
+    UsersModule
+  ],
   controllers: [FaqController],
   providers: [FaqService],
   exports: [FaqService],
